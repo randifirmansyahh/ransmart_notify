1cf308131119b4a584e1643e215fc15cb53d82f2
Randi Firmansyah <randykelvin26@gmail.com>
2022-06-05 09:34:21 +0700

    add to database

--- go.mod
+++ go.mod
@@ -5,9 +5,17 @@ go 1.17
 require github.com/eclipse/paho.mqtt.golang v1.3.5
 
 require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+)
+
+require (
 	github.com/go-chi/chi v1.5.4
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/joho/godotenv v1.4.0
 	github.com/spf13/cast v1.5.0
 	golang.org/x/net v0.0.0-20200425230154-ff2c4b7c35a0 // indirect
+	gorm.io/driver/mysql v1.3.4
+	gorm.io/gorm v1.23.5
 )
--- main.go
+++ main.go
@@ -12,7 +12,23 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
 	"github.com/spf13/cast"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
+
+type Notify struct {
+	Id      int    `json:"id"`
+	OrderId int    `json:"order_id"`
+	Request string `json:"request"`
+	IsSent  bool   `json:"is_sent"`
+	gorm.Model
+}
+
+type ReqNotify struct {
+	OrderId int         `json:"order_id"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
 
 var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
 	fmt.Printf("%s", msg.Payload())
@@ -28,11 +44,23 @@ var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err
 
 func main() {
 
+	// Load env
 	err := godotenv.Load("params/.env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
+	// Connect to database
+	dsn := os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME") + "?charset=utf8&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&Notify{})
+
+	// Create a new MQTT Client
 	var broker = os.Getenv("BROKER")
 	var port = cast.ToInt(os.Getenv("PORT"))
 	opts := mqtt.NewClientOptions()
@@ -48,6 +76,7 @@ func main() {
 		panic(token.Error())
 	}
 
+	// subcribe to topic
 	sub(client)
 
 	// router
@@ -55,22 +84,42 @@ func main() {
 
 	// check service
 	r.Group(func(g chi.Router) {
+		g.Get("/check", func(w http.ResponseWriter, r *http.Request) {
+			response.Response(w, http.StatusOK, "Service is running", nil)
+		})
+
 		g.Post("/publis", func(w http.ResponseWriter, r *http.Request) {
 			decoder := json.NewDecoder(r.Body)
-			var datarequest map[string]interface{}
+			var datarequest ReqNotify
 			if err := decoder.Decode(&datarequest); err != nil {
 				response.Response(w, http.StatusBadRequest, "Invalid request payload", nil)
 				return
 			}
 
+			// to json string
 			newRequst, err := json.Marshal(datarequest)
 			if err != nil {
 				response.Response(w, http.StatusBadRequest, "Invalid request payload", nil)
 				return
 			}
 
+			// Create
+			db.Create(&Notify{OrderId: datarequest.OrderId, Request: string(newRequst)})
+
+			// Publish
 			publish(client, string(newRequst))
+
+			// update is_sent
+			db.Model(&Notify{}).Where("order_id = ?", datarequest.OrderId).Update("is_sent", true)
+
+			// Response
 			response.Response(w, http.StatusOK, "Publish success", datarequest)
+		})
+
+		g.Get("/notifies", func(w http.ResponseWriter, r *http.Request) {
+			var data []Notify
+			db.Find(&data)
+			response.Response(w, http.StatusOK, "Get notifies success", data)
 		})
 	})
 
